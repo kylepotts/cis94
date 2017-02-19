f92e4b9d4f79faef4101bfbd7008d5ce00b83622
Kyle Potts <kyle@kylepotts.com>
2017-02-19 17:17:13 -0500

    complete HW2

--- src/Hw2/Hw2.hs
+++ src/Hw2/Hw2.hs
@@ -17,6 +17,31 @@ parse :: String -> [LogMessage]
 parse [] = []
 parse s  = map parseMessage (lines s)
 
-newNode:: LogMessage -> MessageTree
-newNode a = Node Leaf a Leaf
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert message@(LogMessage _ timestampA _) tree = case tree of
+  (Leaf) -> Node Leaf message Leaf
+  (Node _ (Unknown _) _) -> tree
+  -- If timestamp of message to insert is less then roots timestamp, insert left else insert right
+  (Node left root@(LogMessage _ timestampB _) right) -> case timestampA < timestampB of
+    False -> Node left root (insert message right)
+    True  -> Node (insert message left) root right
 
+build :: [LogMessage] -> MessageTree
+build messages = foldl (\tree message -> insert message tree) Leaf messages
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder tree = case tree of
+  (Leaf) -> []
+  (Node left (Unknown _) right) -> inOrder(left) ++ inOrder(right)
+  (Node left message right) -> inOrder(left) ++ [message] ++ inOrder(right)
+
+isBadError :: LogMessage -> Bool
+isBadError (LogMessage (Error sev) _ _ ) = sev > 49
+isBadError _  = False
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong messages = map (\(LogMessage _ _ message) -> message) 
+  $ inOrder 
+  $ build 
+  $ filter (\ message -> isBadError message) messages
